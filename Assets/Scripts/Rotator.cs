4b7db6fc572bfeddf4add9ae2336e793e55514a4
DESKTOP-A3GV9VV\202_01 <wlsdud369784@gmail.com>
2025-07-03 09:47:18 +0900

    에셋 스토어에서 무료로 에셋 다운

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public bool isRotating = true;
-    [SerializeField] private float rotationSpeed = 50.0f;
+    private float rotationSpeed = 50.0f;
+    [SerializeField] private float minRotationSpeed = -70.0f;
+    [SerializeField] private float maxRotationSpeed = 70.0f;
 
     float changeDelay = 0.0f;
 
@@ -16,8 +18,8 @@ public class Rotator : MonoBehaviour
     public void Init()
     {
         changeDelay = 0.0f;
-        rotationSpeed = Random.Range(-60.0f, 60.0f);
-        isRotating = true;
+        rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        // isRotating = true;
 
         transform.rotation = new Quaternion(0,0,0,0); // Reset rotation to initial state
     }
@@ -30,7 +32,7 @@ public class Rotator : MonoBehaviour
             changeDelay += Time.deltaTime;
             if (changeDelay >= 5.0f)
             {
-                rotationSpeed = Random.Range(-60.0f, 60.0f);
+                rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
                 changeDelay = 0;
             }
 
